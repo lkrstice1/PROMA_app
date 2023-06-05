c762c00b1a90ad302c92fdee3d346437a13fcff7
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-05 22:10:18 +0200

    Dodavanje elemenata aplikacije

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Naslov from './components/Naslov';
+import PocetniEkran from './screens/PocetniEkran';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
+    <View style={stilovi.ekran}>
+      <Naslov naslov={"PROMA"}/>
+      <PocetniEkran />
     </View>
   );
 }
 
 const stilovi = StyleSheet.create({
-  
+  ekran: {
+    flex: 1
+  }
 });
