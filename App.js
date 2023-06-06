52043db9a7fba2a17be92ef515289ab211be545d
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-06 15:26:13 +0200

    Izgled aplikacije

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState }from 'react';
 import { View, StyleSheet } from 'react-native';
 import Naslov from './components/Naslov';
 import PocetniEkran from './screens/PocetniEkran';
+import EkranIgre from './screens/EkranIgre';
+import KrajIgre from './screens/KrajIgre';
 
 export default function App() {
+  const [brKorisnika, postaviBroj] = useState()
+  const [brojRundi, postaviRunde] = useState(0)
+
+  const pocetakIgre = (broj) => {
+    postaviBroj(broj)
+  }
+
+  const krajIgre = (runde) => {
+    postaviRunde(runde)
+  }
+
+  const novaIgra = () => {
+    postaviBroj(null)
+    postaviRunde(0)
+  }
+
+  let sadrzaj = <PocetniEkran pocetak={pocetakIgre}/>
+
+  if(brKorisnika && brojRundi <= 0){
+    sadrzaj = <EkranIgre kraj={krajIgre} broj={brKorisnika} />
+  } else if (brojRundi > 0) {
+    sadrzaj = <KrajIgre nova={novaIgra} broj={brKorisnika} runde={brojRundi}/>
+  }
+  
   return (
-    <View style={stilovi.ekran}>
+    <View style={styles.ekran}>
       <Naslov naslov={"PROMA"}/>
-      <PocetniEkran />
+      {sadrzaj}
     </View>
   );
 }
 
-const stilovi = StyleSheet.create({
+const styles = StyleSheet.create({
   ekran: {
     flex: 1
   }
